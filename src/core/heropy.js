3711fe7d0d2dd0816b20c46dfa1178ddb5ed2942
developer-jyyun <developer.jyyun@gmail.com>
2023-09-10 04:34:26 +0900

    Add Serverless Functions

--- src/core/heropy.js
+++ src/core/heropy.js
@@ -1,121 +1,92 @@
 ///// Component /////
 export class Component {
   constructor(payload = {}) {
-    // 기본값 빈 객체로 할당
     const {
-      //객체 구조분해 할당
-      tagName = "div", // 최상위 요소의 태그 이름 기본값 div로
+      tagName = 'div', // 최상위 요소의 태그 이름
       props = {},
-      state = {},
-    } = payload;
-
-    this.el = document.createElement(tagName); // 컴포넌트의 최상위 요소
-    this.props = props; // 컴포넌트가 사용될 때 부모 컴포넌트에서 받는 데이터
-    this.state = state; // 컴포넌트 안에서 사용할 데이터
-    this.render();
+      state = {}
+    } = payload
+    this.el = document.createElement(tagName) // 컴포넌트의 최상위 요소
+    this.props = props // 컴포넌트가 사용될 때 부모 컴포넌트에서 받는 데이터
+    this.state = state // 컴포넌트 안에서 사용할 데이터
+    this.render()
   }
-  render() {
-    // 컴포넌트를 렌더링하는 함수
+  render() { // 컴포넌트를 렌더링하는 함수
     // ...
   }
 }
+
 
 ///// Router /////
 // 페이지 렌더링!
 function routeRender(routes) {
   // 접속할 때 해시 모드가 아니면(해시가 없으면) /#/로 리다이렉트!
-  // 해쉬 없을 경우 : 매칭가능한 주소x : 출력 할 컴포넌트 찾을 수 없다.
   if (!location.hash) {
-    //location.hash가 빈 문자열이면 (!거짓이면)
-    history.replaceState(null, "", "/#/"); // (상태, 제목, 주소)
+    history.replaceState(null, '', '/#/') // (상태, 제목, 주소)
   }
-  const routerView = document.querySelector("router-view");
-
-  // 물음표를 기준으로 해시 정보와 쿼리스트링을 구분
-  // 배열 구조분해 할당으로 배열의 0번째: hash, 1번째 queryString
-  // #/about?name=heropy :: ["#/about","name=heropy"]
-
-  const [hash, queryString = ""] = location.hash.split("?");
-  //location.hash를 통해 현재 주소 확인
-  //hash부분의 주소만 추출해서(queryString부분 제외한) 주소와 일치하는
-  //routes(index.js의 createRouter 배열 데이터[]) 배열데이터의 내용을
-  //find 메서드로 찾아 currentRoute 변수에 할당
-
-  // 2) 현재 라우트 정보를 찾아서 렌더링!
-  const currentRoute = routes.find((route) => {
-    // /#\/about\?$.test(hash)
-    return new RegExp(`${route.path}/?$`).test(hash);
-  });
-  routerView.innerHTML = "";
-  //currentRoute의 component의 내용을 생성자 함수로 실행
-  routerView.append(new currentRoute.component().el);
+  const routerView = document.querySelector('router-view')
+  const [hash, queryString = ''] = location.hash.split('?') // 물음표를 기준으로 해시 정보와 쿼리스트링을 구분
 
   // 1) 쿼리스트링을 객체로 변환해 히스토리의 상태에 저장!
-  // queryString :: a=123&b=456 key value형태로되어 있다.
-  // 쿼리를 {a:’123’, b:’456’}의 값을 얻도록 해보자!!
-  const query = queryString.split("&").reduce((acc, cur) => {
-    const [key, value] = cur.split("="); // 배열 구조 분해 할당
-    acc[key] = value; // acc객체 데이터의 key에 value를 담아줌
-    return acc; // 누적되는 값을 reduce의 콜백에서 반환
-  }, {});
+  const query = queryString
+    .split('&')
+    .reduce((acc, cur) => {
+      const [key, value] = cur.split('=')
+      acc[key] = value
+      return acc
+    }, {})
+  history.replaceState(query, '') // (상태, 제목)
 
-  // history 객체의 state에 객체 데이터 정보(쿼리스트링 정보)를 담음
-  history.replaceState(query, ""); // (상태, 제목)
+  // 2) 현재 라우트 정보를 찾아서 렌더링!
+  const currentRoute = routes.find(route => new RegExp(`${route.path}/?$`).test(hash))
+  routerView.innerHTML = ''
+  routerView.append(new currentRoute.component().el)
 
   // 3) 화면 출력 후 스크롤 위치 복구!
-  window.scrollTo(0, 0);
+  window.scrollTo(0, 0)
 }
-
 export function createRouter(routes) {
   // 원하는(필요한) 곳에서 호출할 수 있도록 함수 데이터를 반환!
   return function () {
-    window.addEventListener("popstate", () => {
-      //페이지 주소 부분 바뀌면 한번씩 동작
-      routeRender(routes);
-    });
-    routeRender(routes); //최초 호출 코드
-  };
+    window.addEventListener('popstate', () => {
+      routeRender(routes)
+    })
+    routeRender(routes)
+  }
 }
+
 
 ///// Store /////
 export class Store {
   constructor(state) {
-    this.state = {}; // 상태(데이터)
-    this.observers = {};
+    this.state = {} // 상태(데이터)
+    this.observers = {}
     for (const key in state) {
-      //객체 데이터 반복 for key in 객체이름
       // 각 상태에 대한 변경 감시(Setter) 설정!
       Object.defineProperty(this.state, key, {
         // Getter
-        get: () => {
-          //this.state 데이터의 key값 사용할 때
-          return state[key];
-        },
+        get: () => state[key],
         // Setter
-        set: (val) => {
-          // this.state 데이터의 특정 속성의 값 할당 할 때
-          console.log(val);
-          state[key] = val;
-          if (Array.isArray(this.observers[key])) {
-            // 호출할 콜백이 있는 경우!
-            this.observers[key].forEach((observer) => observer(val));
+        set: val => {
+          state[key] = val
+          if (Array.isArray(this.observers[key])) { // 호출할 콜백이 있는 경우!
+            this.observers[key].forEach(observer => observer(val))
           }
-        },
-      });
+        }
+      })
     }
   }
   // 상태 변경 구독!
   subscribe(key, cb) {
-    //삼항 연산자
     Array.isArray(this.observers[key]) // 이미 등록된 콜백이 있는지 확인!
       ? this.observers[key].push(cb) // 있으면 새로운 콜백 밀어넣기!
-      : (this.observers[key] = [cb]); // 없으면 콜백 배열로 할당!
+      : this.observers[key] = [cb] // 없으면 콜백 배열로 할당!
 
     // 예시)
-    //   observers = {
-    //     구독할상태이름: [실행할콜백1, 실행할콜백2]
-    //     movies: [cb, cb, cb],
-    //     message: [cb]
-    //   }
+    // observers = {
+    //   구독할상태이름: [실행할콜백1, 실행할콜백2]
+    //   movies: [cb, cb, cb],
+    //   message: [cb]
+    // }
   }
 }
--- src/store/movie.js
+++ src/store/movie.js
@@ -1,76 +1,58 @@
-import { Store } from "../core/heropy";
+import { Store } from '../core/heropy'
+
 const store = new Store({
-  //생성자 함수 호출__인수로 객체데이터 추가
-  searchText: "",
+  searchText: '',
   page: 1,
   pageMax: 1,
-  movies: [], //검색결과 영화
-  movie: {}, //영화 상세정보
+  movies: [],
+  movie: {},
   loading: false,
-  message: "Search for movie title!",
-});
+  message: 'Search for the movie title!'
+})
 
-export default store;
-export const searchMovies = async (page) => {
-  store.state.loading = true;
-  store.state.page = page;
+export default store
+export const searchMovies = async page => {
+  store.state.loading = true
+  store.state.page = page
   if (page === 1) {
-    store.state.movies = [];
-    store.state.message = "";
+    store.state.movies = []
+    store.state.message = ''
   }
-
   try {
-    //내 api는 왜 에러나지..?
-    //`https://www.omdbapi.com/?i=tt3896198&apikey=5d29f53c&s=${store.state.searchText}&page=${page}`
-    //쌤: `https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`
-
-    const res = await fetch(
-      `https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`
-    );
-
-    //객체구조분해할당
-    //네트워크 탭의 fetch/XHR. 미리보기 탭에서 totalResult, Response, Error 속성 확인 가능.
-    const { Search, totalResults, Response, Error } = await res.json();
-    // 영화 정보 정상적으로 가져온다면
-    if (Response === "True") {
-      //배열의 내용이 계속 누적 될 수 있도록 전개 연산자 사용.
-      store.state.movies = [...store.state.movies, ...Search];
-      store.state.pageMax = Math.ceil(Number(totalResults) / 10);
+    const res = await fetch('/api/movie', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: store.state.searchText,
+        page
+      })
+    })
+    const { Response, Search, totalResults, Error } = await res.json()
+    if (Response === 'True') {
+      store.state.movies = [
+        ...store.state.movies,
+        ...Search
+      ]
+      store.state.pageMax = Math.ceil(Number(totalResults) / 10)
     } else {
-      store.state.message = Error;
-      store.state.pageMax = 1;
+      store.state.message = Error
+      store.state.pageMax = 1
     }
   } catch (error) {
-    console.log("searchMovies error", error);
+    console.log('searchMovies error:', error)
   } finally {
-    store.state.loading = false;
+    store.state.loading = false
   }
-};
-
-//상세페이지
-// `https://www.omdbapi.com/?i=tt3896198&apikey=5d29f53c&i=${id}&plot=full` 내 api는 왜 에러나지..?
-// 쌤: https://omdbapi.com?apikey=7035c60c&i=${id}&plot=full
-export const getMovieDetails = async (id) => {
+}
+export const getMovieDetails = async id => {
   try {
-    const res = await fetch(
-      `https://omdbapi.com?apikey=7035c60c&i=${id}&plot=full`
-    );
-    store.state.movie = await res.json();
-  } catch {
-    console.log("상세 페이지 에러", error);
+    const res = await fetch('/api/movie', {
+      method: 'POST',
+      body: JSON.stringify({
+        id
+      })
+    })
+    store.state.movie = await res.json()
+  } catch (error) {
+    console.log('getMovieDetails error:', error)
   }
-};
-// export const getMovieDetails = async (id) => {
-//   try {
-//     // const res = await fetch(`https://omdbapi.com?apikey=${APIKEY}&i=${id}&plot=full`)
-//     const res = await fetch("/api/movie", {
-//       method: "POST",
-//       body: JSON.stringify({
-//         id,
-//       }),
-//     });
-//     store.state.movie = await res.json();
-//   } catch (error) {
-//     console.log("getMovieDetails error:", error);
-//   }
-// };
+}
